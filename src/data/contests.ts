c74ce40e3a61542905abdd2281696dbc95aceab9
Simon Ashton <simon@smntic.dev>
2025-08-25 21:41:41 -0700

    Add CF Round 1043

--- src/data/contests.ts
+++ src/data/contests.ts
@@ -53,5 +53,26 @@ export const contests: Contest[] = [
       { name: "Triple Sevens", index: "M", status: ProblemStatus.Solved, notes: "First problem we solved. We need to be able to find these simple problems first and implement them quickly to minimize penalty." },
     ],
   },
+
+  {
+    name: "CF Round 1043",
+    notes: `
+      My 87th Codeforces contest. Second best rank of all time, though it was a div 3.
+      Also, my first time participating as specialist since February. I thought the
+      problems were a little hard for a div 3, but they were all very classical.
+      I performed well mainly because I got lucky with the problemset, but I was also
+      focused throughout the contest and always working on something.
+    `,
+    problems: [
+      { name: "Homework", index: "A", status: ProblemStatus.Solved, notes: "Brute force simulation. Hardest part is reading the problem statement." },
+      { name: "The Secret Number", index: "B", status: ProblemStatus.Solved, notes: "The problem statement is equivalent to a purely mathematical equality. Use that to find the value x." },
+      { name: "The Cunning Seller (easy version)", index: "C1", status: ProblemStatus.Solved, notes: "Every number can be expressed in base N by greedily subtracting the highest possible power of N from the number." },
+      { name: "The Cunning Seller (hard version)", index: "C2", status: ProblemStatus.Solved, notes: "Let f(x) be the cost function for 3^x watermelons. Then the cost function satisfies f(x)-3f(x-1) is positive and increasing. Thus, find the construction from C1 and greedily convert deals with the highest values of x to deals of x-1. I got TLE on my first submission because I forgot about the exponential increase in number of deals and the large value of k." },
+      { name: "From 1 to Infinity", index: "D", status: ProblemStatus.NeedsReview, notes: "Binary search to find which number the string ends with, then count the digit sum of numbers before that and the prefix of the last number that is still within the string. There is a formula to find the sum of all digits of numbers between 1 and n. I should derive the formula myself and understand whether there are better ways to approach the problem." },
+      { name: "Arthmetics Competition", index: "E", status: ProblemStatus.Solved, notes: "Classic binary search problem. It is clearly optimal to always take z_i cards. Define a function f(k) to be the maximum possible sum when taking k cards from pile a. The function is concave (removing low cards from a while adding high cards from b), so binary search for the maximum value. Need to check adjacent values as well depending on binary search approach." },
+      { name: "Rada and the Chamomile Valley", index: "F", status: ProblemStatus.Solved, notes: "Classic graph problem. The \"lanes that they will definitely\" use are the bridges that are on any path from 1 to n. Find the bridges, then take any path's edges and find the intersection of the two sets. Then, use multi-source BFS to find the answers for all the nodes. The queries can then be solved by reporting those answers." },
+      { name: "Famous Choreographer", index: "G", status: ProblemStatus.Solved, notes: "Solved after the contest. Main issue was reading the problem statement, but the samples and explanations were key. Observe that the original matrix will be placed at one of the corners of the new matrix. Also, adding rows to the bottom and right sides of the matrix is equivalent to removing rows from the top and left sides. Thus, for each of the four rotations of the matrix, find the minimum answer over all possible ways to remove those rows. Note that checking 180 degree equivalence is equivalent to checking if the string formed by reading the submatrix in row order is a palindrome. Can use hashing to check that. Store the hashes for the forward and reverse directions of the string formed by the submatrix, then use those to find the answer. Also need to use a strong hashing technique to avoid hacks during the open hacking period." },
+    ],
+  },
 ];
 
